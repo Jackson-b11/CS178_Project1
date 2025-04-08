d277836cbcdd873c46467ef6c86a91af1047e58b
Jackson <beckerjackson111@gmail.com>
2025-04-08 11:27:14 -0500

    deleted python testing

---
+++ FlaskApp.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key' # this is an artifact for using flash displays; 
+                                   # it is required, but you can leave this alone
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# these two lines of code should always be the last in the file
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
